1083f841214084ce29b6967ed3b1b86fe95406aa
naoki hirata <naoki@magic3.org>
2022-11-17 12:22:52 +0900

    Nuxt3をv3.0.0にバージョンアップ。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,9 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
-export default defineNuxtConfig({
-  //modules: ['@nuxtjs/tailwindcss'],
+export default {
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
   colorMode: {
     preference: 'system', // default theme
     dataValue: 'theme', // activate data-theme in <html> tag
     classSuffix: '',
   },
-})
+}
