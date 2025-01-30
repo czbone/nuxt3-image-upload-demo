3324b20bfd785a50de6e6e3874818bba684f621d
naoki hirata <naoki@magic3.org>
2025-01-31 07:36:37 +0900

    パッケージ更新。

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default {
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
+
   colorMode: {
     preference: 'system', // default theme
     dataValue: 'theme', // activate data-theme in <html> tag
     classSuffix: '',
   },
-}
+
+  compatibilityDate: '2025-01-31',
+};
